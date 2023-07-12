bfefda08c7a6ae14ac8a8a000a6e12aff44acb0f
Christopher James <68414004+gamingbuddhist@users.noreply.github.com>
2023-07-11 23:47:52 -0400

    add keyboard search ergonomics - arrow keys - autofocus search on typing - mouseover results

--- JS/search.js
+++ JS/search.js
@@ -1,5 +1,10 @@
 const search = document.getElementById('searchInput');
 
+let resultsDivs = [];
+let selectedIndex = 0;
+let prevX;
+let prevY;
+
 function searchDocs() {
     let results = new Map();
     let query = search.value;
@@ -34,6 +39,8 @@ search.addEventListener('input', (e) => {
     }
     toast.style.display = "none";
     const results = searchDocs();
+    resultsDivs = [];
+    selectedIndex = 0;
     if (results.size > 0) {
         main.innerHTML = "";
         results.forEach(([path, name, kind], fullName) => {
@@ -50,10 +57,74 @@ search.addEventListener('input', (e) => {
             let p = document.createElement('p');
             p.appendText(fullName, "description");
             resultDiv.appendChild(p);
+            
+            resultDiv.addEventListener('click', (e) => {
+                resultLink.click();
+            })
+            resultDiv.addEventListener('mouseover', (e) => {
+                if (e.x === prevX && e.y === prevY) {
+                    return;
+                }
+                prevX = e.x;
+                prevY = e.y;
+                resultsDivs[selectedIndex].classList.remove('focused-result');
+                selectedIndex = resultsDivs.indexOf(resultDiv);
+                selectResult(0, false);
+            })
 
             main.appendChild(resultDiv);
+            resultsDivs.push(resultDiv);
+            resultsDivs[0].classList.add('focused-result');
         })
     } else {
         main.innerHTML = "No results found";
     }
 })
+
+window.addEventListener('keypress', (e) => {
+    search.focus();
+})
+
+window.addEventListener('keydown', (e) => {
+    if (search.value.length === 0) {
+        return;
+    }
+    if (e.key === "ArrowUp" && selectedIndex > 0) {
+        e.preventDefault();
+        selectResult(-1);
+    } else if (e.key === "ArrowDown" && selectedIndex < resultsDivs.length - 1) {
+        e.preventDefault();
+        selectResult(1);
+    } else if (e.key === "Backspace") {
+        search.focus();
+    } else if (e.key === "Enter") {
+        e.preventDefault();
+        resultsDivs[selectedIndex].querySelector('a').click();
+    }
+})
+
+search.addEventListener('keydown', (e) => {
+    if (e.key === "ArrowUp" && selectedIndex > 0) {
+        e.preventDefault();
+        search.blur();
+    } else if (e.key === "ArrowDown" && selectedIndex < resultsDivs.length - 1) {
+        e.preventDefault();
+        search.blur();
+    }
+})
+
+function selectResult(decrement= 0, scroll=true) {
+    if (resultsDivs.length === 0) {
+        return;
+    }
+    
+    if (decrement !== 0) {
+        resultsDivs[selectedIndex].classList.remove('focused-result');
+        selectedIndex += decrement;
+    }
+    
+    resultsDivs[selectedIndex].classList.add('focused-result');
+    if (scroll) {
+        resultsDivs[selectedIndex].scrollIntoView({ block: "nearest" });
+    }
+}
